f3f05bcc6c04e664ecd4f45fde8970e4cd05de80
Karthikeya2884 <karthikeya04b@gmail.com>
2025-05-11 20:56:13 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.database import Base, engine
-from app.sockets import socket_manager
+from app.sockets import create_socket_manager
 from app.routes import router
 
 # Initialize database
@@ -9,8 +9,8 @@ Base.metadata.create_all(bind=engine)
 # Initialize FastAPI app
 app = FastAPI()
 
-# Include WebSocket Manager
-socket_manager.init_app(app)
+# Initialize WebSocket Manager with the app
+socket_manager = create_socket_manager(app)
 
 # Include API Routes
 app.include_router(router)
