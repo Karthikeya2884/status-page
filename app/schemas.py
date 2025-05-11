a5d30a5de27526f740b410b178d3e727d596f825
Karthikeya2884 <karthikeya04b@gmail.com>
2025-05-11 21:13:57 +0530

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,8 +1,19 @@
 from pydantic import BaseModel
 
-class ServiceCreate(BaseModel):
+class ServiceBase(BaseModel):
     name: str
     status: str
+
+class ServiceCreate(ServiceBase):
+    pass
+
+class Service(ServiceBase):
+    id: int
+    organization_id: int
+
+    class Config:
+        orm_mode = True
+
 class IncidentCreate(BaseModel):
     title: str
     description: str
